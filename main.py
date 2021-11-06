211e1b1a3d0d710876de4b4079edadf0bfe50de0
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-11-07 00:52:39 +0300

    tüm hareketler tanımlandı

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ alan = (x_max,y_max)
 class Avci():
     hareket = 1
     avlanma = 8
-    konum = [rd.randrange(1,500,1),rd.randrange(1,500,1)]
+    konum = [[rd.randrange(1,500,1),rd.randrange(1,500,1)]]
 
 class HayvanlarOzellıklerı():
     cıftlesme = 3
@@ -85,7 +85,7 @@ class tavuk_horoz (HayvanlarOzellıklerı):
             elif cinsiyet == "disi":
                 for i in range(0, (self.disi)):
                     self.konumlar_disi.append( [rd.randrange(0, 500, 1), rd.randrange(0, 500, 1)])
-
+avci = Avci()
 koyun =koyun( disi=15, erkek=15)
 inek = inek(  disi=5, erkek=5)
 kurt = kurt( disi=5, erkek=5)
@@ -97,14 +97,29 @@ inek.konumlandırma()
 tavuk_horoz.konumlandırma()
 kurt.konumlandırma()
 tavuk_horoz.konumlandırma()
-
-
-
-print(koyun.konumlar_erkek)
+#hareketler
+sagsol(konumlar=avci.konum,hareket=avci.hareket)
 sagsol(konumlar=koyun.konumlar_erkek,hareket=2)
-print(koyun.konumlar_erkek)
+sagsol(konumlar=koyun.konumlar_disi,hareket=2)
+sagsol(konumlar=inek.konumlar_erkek,hareket=2)
+sagsol(konumlar=inek.konumlar_disi,hareket=2)
+sagsol(konumlar=kurt.konumlar_erkek,hareket=3)
+sagsol(konumlar=kurt.konumlar_disi,hareket=3)
+sagsol(konumlar=aslan.konumlar_erkek,hareket=4)
+sagsol(konumlar=aslan.konumlar_disi,hareket=4)
+sagsol(konumlar=tavuk_horoz.konumlar_erkek,hareket=1)
+sagsol(konumlar=tavuk_horoz.konumlar_disi,hareket=1)
 
-#a = [13, 15]
-#print(koyun.konumlar_erkek.__len__())
+ileriasagi(konumlar=avci.konum,hareket=avci.hareket)
+ileriasagi(konumlar=koyun.konumlar_erkek,hareket=2)
+ileriasagi(konumlar=koyun.konumlar_disi,hareket=2)
+ileriasagi(konumlar=inek.konumlar_erkek,hareket=2)
+ileriasagi(konumlar=inek.konumlar_disi,hareket=2)
+ileriasagi(konumlar=kurt.konumlar_erkek,hareket=3)
+ileriasagi(konumlar=kurt.konumlar_disi,hareket=3)
+ileriasagi(konumlar=aslan.konumlar_erkek,hareket=4)
+ileriasagi(konumlar=aslan.konumlar_disi,hareket=4)
+ileriasagi(konumlar=tavuk_horoz.konumlar_erkek,hareket=1)
+ileriasagi(konumlar=tavuk_horoz.konumlar_disi,hareket=1)
 
 
